5e5f71dccd9d5bf37b2834a8e77c0f75d55fea9f
Karl Odie <karlodie@hotmail.co.uk>
2024-08-29 09:57:59 +0100

    MUI functional

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import ButtonAppBar from './navbar';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<h1>Hello, react!</h1>);
+root.render(
+  <div>
+    <ButtonAppBar />
+    <h1>Hello, react!</h1>
+  </div>,
+);
