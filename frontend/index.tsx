fb1cca07f70c5c02cf346333bc752c0f9da4642a
Karl Odie <karlodie@hotmail.co.uk>
2024-08-29 18:16:13 +0100

    Streamlined frontend template

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import ButtonAppBar from './navbar';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+document.body.style.margin = '0px';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <div>
+  <>
     <ButtonAppBar />
     <h1>Hello, react!</h1>
-  </div>,
+  </>,
 );
