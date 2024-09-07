cf9a4247b6f29bdcbc4210a8f79a2ce22935993a
Karl Odie <karlodie@hotmail.co.uk>
2024-09-07 18:31:52 +0100

    Don't precommit auto-generated API

--- sync_api.ps1
+++ sync_api.ps1
@@ -1,2 +1,2 @@
-. docker compose -f .\docker-compose.local.yml run --rm django python manage.py spectacular --file api_schema.yml
+. docker compose -f .\docker-compose.local.yml run --rm django python manage.py spectacular --file portfolio/api/schema.yml
 . docker compose -f .\docker-compose.local.yml run --rm node npm run sync-api
